90c754ef46848e16b93a621d641dd3f57a25ed06
adityasinghrajawat1 <adityasinghraj0405@gmail.com>
2025-03-28 10:30:48 +0530

    Initial commit

---
+++ src/main/java/com/ms/GitApp1Application.java
@@ -0,0 +1,13 @@
+package com.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitApp1Application.class, args);
+	}
+
+}
