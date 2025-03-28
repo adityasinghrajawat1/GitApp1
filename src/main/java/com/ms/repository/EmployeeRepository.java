f011f1db7db6c2e2db1f1f7217ecd4a5201606f0
adityasinghrajawat1 <adityasinghraj0405@gmail.com>
2025-03-28 13:01:10 +0530

    add a package repository and create a interface EmployeeRepository

---
+++ src/main/java/com/ms/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.ms.repository;
+
+import com.ms.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Integer>
+{
+
+}
